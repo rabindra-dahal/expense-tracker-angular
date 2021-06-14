d1febd84ef03f85e850117db228251d66574735a
Rabindra <rabindra.dahal@ut.ee>
2021-06-14 21:09:51 +0300

    Router module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule,Routes} from "@angular/router"
 
 import { AppComponent } from './app.component';
 import { ListExpensesComponent } from './components/list-expenses/list-expenses.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+
+const routers: Routes = [
+  {path:'expenses',component:ListExpensesComponent},
+  {path:'addexpense',component:AddExpenseComponent},
+  {path:'',redirectTo:'/expenses',pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListExpensesComponent
+    ListExpensesComponent,
+    AddExpenseComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(routers)
   ],
   providers: [],
   bootstrap: [AppComponent]
